fe9b12ac7a033d187e4d59a0a470312f222cbb09
Spencer Tang <spencer_tang@csilvm-14.cs.ucsb.edu>
2023-01-30 12:48:53 -0800

    Compilable parallel tree adder

--- treesum_mpi.c
+++ treesum_mpi.c
@@ -27,5 +27,25 @@ int global_sum(int my_int /* in */, int my_rank /* in */, int comm_sz /* in */,
 
   /* Your solution */
 
+  int me = my_rank;
+  int numProc = MPI_Comm_size(comm, &comm_sz);
+  int sum = my_int;
+  int other_sum = 0;
+  MPI_Status status;
+  if(me %2 !=0 &&me>0)
+    MPI_Send(&sum, 1, MPI_INT, me -1, 0, comm);
+  for(int gap=1; gap < numProc; gap =gap*2){
+    if(me%(2*gap) ==0){
+      MPI_Recv(&other_sum, 1, MPI_INT, me+gap, 0, comm, MPI_STATUS_IGNORE);
+    sum= other_sum+sum;
+    if( (me % (4*gap)) !=0 && me>=2*gap)
+      MPI_Send(&sum, 1, MPI_INT, me -2*gap, 0, comm);
+    }
+  }
+  if(numProc%2 == 1) {  // If odd num Processors, the last value is added afterwards
+    MPI_Recv(&other_sum, 1, MPI_INT, numProc -1, 0, comm, MPI_STATUS_IGNORE);
+    sum= other_sum+sum;
+  }
+
   return my_sum;
 } /* Global_sum */
