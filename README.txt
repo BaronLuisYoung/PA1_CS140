011f21ef2e297c4f9244e17d6723211cd07894e0
Spencer Tang <spencer_tang@ecellvm-02.engr.ucsb.edu>
2023-02-01 22:13:20 -0800

    Working tree sum

--- README.txt
+++ README.txt
@@ -10,6 +10,16 @@ Email of Student 2:
 Report for Question 1
 
 
+4 cores: 
+    Wall clock time = 7.990034
+    pi_estimate = 3.141605
+    Error = 0.000013
+
+8 cores:
+    Wall = 3.321255
+    pi estimate = 3.141587
+    Error = 0.000006
+
 
 Report for Question 3.a
 
--- treesum_mpi.c
+++ treesum_mpi.c
@@ -27,44 +27,18 @@ int global_sum(int my_int /* in */, int my_rank /* in */, int comm_sz /* in */,
 
   /* Your solution */
 
-  // // printf("I am proc #%d, and this is my value %d\n", my_rank, my_int);
-
-  int numProc = comm_sz;
+  int gap = 1;
   int sum = my_int;
-  int other_sum = 0;
-
-
-  // printf("NUM PROC: %d\n", numProc);
-  if(numProc%2 == 1 && my_rank == (numProc -1)) {  // If odd num Processors, the last value is added afterwards
-      MPI_Send(&sum, 1, MPI_INT, 0, 0, comm);
-    // printf("Sum %d\n", sum);
-  }
-  // printf("Sum %d\n", sum);
-  // printf("MySum %d\n", my_sum);
-  if(numProc%2 == 1 && my_rank == 0) {
-    MPI_Recv(&other_sum, 1, MPI_INT, numProc -1, 0, comm, MPI_STATUS_IGNORE);
-    sum= other_sum+sum;
-  }
-
-  // // printf("%d, this is my_rank!\n", my_rank);
-  if(my_rank %2 !=0 &&my_rank>0) {
-    MPI_Send(&sum, 1, MPI_INT, my_rank -1, 0, comm);
-  }
-  // printf("Sending 1\n");
-  // printf("NumProc: %d", numProc);
-  for(int gap=1; gap < numProc; gap =gap*2) {
-    // printf("Condition %d\n", my_rank%(2*gap));
-    if(my_rank%(2*gap) == 0 && my_rank+gap < (numProc -1)) {
+  while(my_rank % (gap*2) == 0 && gap < comm_sz) {
+    int other_sum;
+    if(my_rank+gap < comm_sz) {
       MPI_Recv(&other_sum, 1, MPI_INT, my_rank+gap, 0, comm, MPI_STATUS_IGNORE);
-      // printf("Receiving 1\n");
-      sum= other_sum+sum;
-      if( (my_rank % (4*gap)) !=0 && my_rank>=2*gap) {
-        MPI_Send(&sum, 1, MPI_INT, my_rank -2*gap, 0, comm);
-      }
-      // printf("Sending 2\n");
+      sum+=other_sum;
     }
+    gap*=2;      
   }
-
-
+  if(my_rank != 0) {
+    MPI_Send(&sum, 1, MPI_INT, my_rank - gap, 0, comm);
+  }
   return sum;
 } /* Global_sum */
